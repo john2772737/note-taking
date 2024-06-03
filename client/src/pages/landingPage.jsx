b02ea6760614210dd8e02b8a15bb74839a7db018
JOHN <johnregulacion5555@gmail.com>
2024-06-03 18:41:07 +0800

    SDS

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useCallback, useEffect, useState } from "react"
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+import '../pages/style.css'
 
-function landingPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+const SAVE_INTERVAL_MS = 2000
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
+
+export default function LandingPage() {
+  
+ 
+
+  
+ 
+  const wrapperRef = useCallback(wrapper => {
+    if (wrapper == null) return
+
+    wrapper.innerHTML = ""
+    const editor = document.createElement("div")
+    wrapper.append(editor)
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
+   
+  }, [])
+  return <div className="container" ref={wrapperRef}></div>
 }
-
-export default landingPage
